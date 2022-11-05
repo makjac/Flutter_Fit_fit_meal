d212ddcb7fe90b47e71bf04fa6e014c95afad2ee
makjac <56642363+makjac@users.noreply.github.com>
2022-11-06 00:28:27 +0100

    results

--- lib/screens/home/pages/products_result/products_results.dart
+++ lib/screens/home/pages/products_result/products_results.dart
@@ -1,4 +1,5 @@
 import 'package:fit_fit_meal/bloc/product/product_bloc.dart';
+import 'package:fit_fit_meal/utils/insets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -10,24 +11,111 @@ class ProductsResults extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Results"),
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
       ),
-      body: BlocBuilder<ProductBloc, ProductState>(
-        builder: (context, state) {
-          if (state is ProductsLoaded) {
-            return const Text("loaded");
-          }
-          if (state is LoadingProducts) {
-            return const Center(
-              child: CircularProgressIndicator(
-                color: Colors.red,
-              ),
-            );
-          }
-          return const Center(
-            child: Text("something is wrong"),
-          );
-        },
+      backgroundColor: Colors.orange,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(Insets.xs),
+          child: BlocBuilder<ProductBloc, ProductState>(
+            builder: (context, state) {
+              if (state is ProductsLoaded) {
+                // return const Text("loaded");
+                return Column(
+                  children: state.products
+                      .map(
+                        (product) => Padding(
+                          padding: const EdgeInsets.all(Insets.xs),
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              foregroundColor: Colors.red,
+                              backgroundColor: Colors.white,
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(Insets.xs),
+                              child: Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(Insets.xs),
+                                    child: Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          product.name,
+                                          style: const TextStyle(
+                                            fontSize: 35,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        const SizedBox(width: Insets.xs),
+                                        Text(
+                                          product.producer,
+                                          style: const TextStyle(
+                                            fontSize: 25,
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      _productRow("Energy",
+                                          "${product.nutritionalLabelling.energy} kcal"),
+                                      _productRow("Fat",
+                                          "${product.nutritionalLabelling.fat}"),
+                                      _productRow("Saturated",
+                                          "${product.nutritionalLabelling.saturated}"),
+                                      _productRow("Carbohydrates",
+                                          "${product.nutritionalLabelling.carbohydrates}"),
+                                      _productRow("Sugar",
+                                          "${product.nutritionalLabelling.sugar}"),
+                                      _productRow("Salt",
+                                          "${product.nutritionalLabelling.salt}"),
+                                      _productRow("Protein",
+                                          "${product.nutritionalLabelling.protein}"),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                );
+              }
+              if (state is LoadingProducts) {
+                return const Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.red,
+                  ),
+                );
+              }
+              return const Center(
+                child: Text("something is wrong"),
+              );
+            },
+          ),
+        ),
       ),
     );
   }
+
+  Widget _productRow(String label, String value) => Padding(
+        padding: const EdgeInsets.symmetric(horizontal: Insets.l),
+        child: Row(
+          children: [
+            Expanded(flex: 3, child: Text(label)),
+            Expanded(
+              child: Text(value),
+            ),
+          ],
+        ),
+      );
 }
