66f614a6a69b65c986df25812a5e3698fc428782
makjac <56642363+makjac@users.noreply.github.com>
2022-11-10 23:18:37 +0100

    update

--- lib/screens/home/pages/scaner_home/pages/product_page/product_page.dart
+++ lib/screens/home/pages/scaner_home/pages/product_page/product_page.dart
@@ -1,10 +1,12 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:fit_fit_meal/screens/home/pages/scaner_home/widgets/nutrition_facts_label.dart';
-import 'package:fit_fit_meal/screens/home/pages/scaner_home/widgets/product_header.dart';
-import 'package:fit_fit_meal/utils/insets.dart';
 import 'package:flutter/material.dart';
 
 import 'package:fit_fit_meal/data/models/product_model.dart';
+import 'package:fit_fit_meal/screens/home/pages/scaner_home/widgets/nutrition_facts_label.dart';
+import 'package:fit_fit_meal/screens/home/pages/scaner_home/widgets/product_header.dart';
+import 'package:fit_fit_meal/utils/insets.dart';
+import 'package:flutter/services.dart';
 
 class ProductPage extends StatelessWidget {
   final Product product;
@@ -33,6 +35,35 @@ class ProductPage extends StatelessWidget {
                 child: Column(
                   children: <Widget>[
                     ProductHeader(product: product),
+                    const SizedBox(height: Insets.s),
+                    Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(30),
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(10),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(Insets.s),
+                        child: Column(
+                          children: <Widget>[
+                            const Text("xyz"),
+                            Row(
+                              children: [
+                                const Text("grams"),
+                                const SizedBox(width: Insets.xs),
+                                Expanded(
+                                  child: TextFormField(),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
                     const SizedBox(height: Insets.s),
                     NutritonFactsLabel(product: product),
                   ],
