5bdd7700479a00c8498e76fd8b17e3e08a492073
makjac <56642363+makjac@users.noreply.github.com>
2024-08-13 00:56:02 +0200

    refactor: Update imports and dependencies in welcome_page.dart

--- lib/features/welcome/welcome_page.dart
+++ lib/features/welcome/welcome_page.dart
@@ -1,6 +1,6 @@
-import 'package:fit_fit_meal/screens/welcome/welcome_sign_up_button.dart';
-import 'package:fit_fit_meal/screens/welcome/welcome_sign_in_button.dart';
-import 'package:fit_fit_meal/screens/welcome/welcome_title.dart';
+import 'package:fit_fit_meal/features/welcome/welcome_sign_in_button.dart';
+import 'package:fit_fit_meal/features/welcome/welcome_sign_up_button.dart';
+import 'package:fit_fit_meal/features/welcome/welcome_title.dart';
 import 'package:fit_fit_meal/widgets/boxDecoration/home_gradnient_bacground.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -14,30 +14,31 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.red,
-        body: BlocListener<AuthBloc, AuthState>(
-          listener: (context, state) {
-            if (state is UserIsAvailable) {
-              context.go("/home");
-            }
-          },
-          child: Center(
-            child: Container(
-              width: double.infinity,
-              decoration: homeGradientBacground(),
-              child: const SafeArea(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    WelcomeTitle(),
-                    SizedBox(height: 150),
-                    WelcomeSignUpButton(),
-                    WelcomeSignInButton(),
-                  ],
-                ),
+      backgroundColor: Colors.red,
+      body: BlocListener<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is UserIsAvailable) {
+            context.go("/home");
+          }
+        },
+        child: Center(
+          child: Container(
+            width: double.infinity,
+            decoration: homeGradientBacground(),
+            child: const SafeArea(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  WelcomeTitle(),
+                  SizedBox(height: 150),
+                  WelcomeSignUpButton(),
+                  WelcomeSignInButton(),
+                ],
               ),
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
