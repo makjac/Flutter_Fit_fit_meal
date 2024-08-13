a5dfe6ae5b152ac23e06b254c19342fcb7cb7b8f
makjac <56642363+makjac@users.noreply.github.com>
2024-08-13 02:21:14 +0200

    chore: Update copyWith method in ActivityItem class

--- lib/data/models/activity_item.dart
+++ lib/data/models/activity_item.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
 
 class ActivityItem extends Equatable {
@@ -12,11 +11,11 @@ class ActivityItem extends Equatable {
     required this.value,
   });
 
-  ActivityItem copyWith(
+  ActivityItem copyWith({
     String? lottiePath,
     String? label,
     double? value,
-  ) {
+  }) {
     return ActivityItem(
       lottiePath: lottiePath ?? this.lottiePath,
       label: label ?? this.label,
